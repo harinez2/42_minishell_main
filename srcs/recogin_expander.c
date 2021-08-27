05493e00dce1d84152b2b50177915266d918ad3d
Ryo Tomiki <ryotomiki2011@gmail.com>
2021-08-27 11:08:18 +0900

    expander & new linked list

--- srcs/recogin_expander.c
+++ srcs/recogin_expander.c
@@ -2,7 +2,15 @@
 
 void	expander_char(t_arg *arg, int option)
 {
-	(void)arg;
+	int	cnt;
+
+	cnt = 0;
+	while(arg->envp[cnt])
+	{
+///		printf("%s\n%d\n", arg->envp[cnt], cnt);
+
+		cnt ++;
+	}
 	(void)option;
 }
 
@@ -14,10 +22,27 @@ void	expander_multiple_char(t_arg *arg)
 	while (arg->cmdlst->param[cnt])
 	{
 		expander_char(arg, cnt);
+		cnt ++;
 	}
 }
 
 void	expander(t_arg *arg)
 {
 	expander_multiple_char(arg);
+
+    extern char **environ;
+    char **env = environ;
+
+    while(*env) {
+        const char *val;
+
+        val = *env;
+        if (val) {
+            printf("%s\n", val);
+        }
+		env++;
+	}
+	const char *value;
+	value = getenv("PWD");
+	printf("-----------\n%s\n------------\n", value);
 }
