511129a8cfa09025e3a1c77b384721ae828d3019
Ryo Tomiki <ryotomiki2011@gmail.com>
2021-08-28 13:26:17 +0900

    env list created

--- srcs/recogin_expander.c
+++ srcs/recogin_expander.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
-void	expander_char(t_arg *arg, int option)
+void	expander_char(char **text)
 {
-	int	cnt;
+	int		cnt;
+	char	*tmp;
 
 	cnt = 0;
-	while(arg->envp[cnt])
+	tmp = *text;
+	while(tmp[cnt])
 	{
-///		printf("%s\n%d\n", arg->envp[cnt], cnt);
 
 		cnt ++;
 	}
@@ -17,19 +18,101 @@ void	expander_char(t_arg *arg, int option)
 void	expander_multiple_char(t_arg *arg)
 {
 	int	cnt;
-	
+
 	cnt = 0;
 	while (arg->cmdlst->param[cnt])
 	{
-		expander_char(arg, cnt);
+		expander_char(&(arg->cmdlst->param[cnt]));
 		cnt ++;
 	}
 }
 
+ssize_t	ft_strchr(const char *s, int c)
+{
+	ssize_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+static char	*ft_strdup2(const char *s1, ssize_t n)
+{
+	ssize_t	i;
+	char	*p;
+
+	p = (char *)malloc(sizeof(char) * (n + 1));
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0' && i < n)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+void	ft_push_back_list(t_env	**envlst, char *env, char *value)
+{
+	t_env	*new_env;
+	t_env	*head;
+
+	new_env = malloc(sizeof(t_env));
+	if (!new_env)
+		exit (1); /// please change this
+	new_env->env = env;
+	new_env->value = value;
+	new_env->next = NULL;
+	head = *envlst;
+	if (!head)
+	{
+		(*envlst) = new_env;
+	}
+	else
+	{
+		while (head->next != NULL)
+			head = head->next;
+		head->next = new_env;	
+	}
+}
+
+void	init_env(t_arg *arg)
+{
+	int		cnt;
+	int		dest;
+	char	*env;
+	char	*value;
+
+	cnt = 0;
+	arg->envlst = NULL;
+	while (arg->envp[cnt])
+	{
+		dest = ft_strchr(arg->envp[cnt], '=');
+		env = ft_strdup2(arg->envp[cnt], dest);
+		value = ft_strdup2(&(arg->envp[cnt][dest + 1]), ft_strlen(arg->envp[cnt]) - dest);
+		ft_push_back_list(&(arg->envlst), env, value);
+		cnt ++;
+	}
+/*	while (arg->envlst->next != NULL)
+	{
+		printf("%s  |  %s\n", arg->envlst->env, arg->envlst->value);
+		arg->envlst = arg->envlst->next;
+	}*/
+}
+
 void	expander(t_arg *arg)
 {
+	init_env(arg);
 	expander_multiple_char(arg);
 
+/*
     extern char **environ;
     char **env = environ;
 
@@ -44,5 +127,5 @@ void	expander(t_arg *arg)
 	}
 	const char *value;
 	value = getenv("PWD");
-	printf("-----------\n%s\n------------\n", value);
+	printf("-----------\n%s\n------------\n", value);*/
 }
