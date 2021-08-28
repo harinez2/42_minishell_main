6a815e105b2b7798ae4a556b2a0295a6186709fa
harinez2 <yonishi@student.42tokyo.jp>
2021-08-28 12:14:21 +0900

    for norm

--- srcs/executer_util.c
+++ srcs/executer_util.c
@@ -2,20 +2,20 @@
 
 void	print_cmdstart(char *s)
 {
-	write(2, "/--Executing:", 13);
+	write(2, "/---Executing:", 14);
 	write(2, s, ft_strlen(s));
 	write(2, "\n", 1);
 }
 
 void	print_cmdend(int status)
 {
-	printf("\\--Exited (Is exited: %x", WIFEXITED(status));
+	printf("\\---Exited (Is exited: %x", WIFEXITED(status));
 	if (WIFEXITED(status))
 		printf(", Exit status: %d", WEXITSTATUS(status));
 	printf(")\n");
 }
 
-void check_and_exit_program(t_arg *arg, t_cmd *c)
+void	check_and_exit_program(t_arg *arg, t_cmd *c)
 {
 	if (ft_strncmp("exit", c->param[0], 5) == 0)
 	{
