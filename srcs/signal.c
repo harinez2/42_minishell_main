fc34bf437da164bf893cfd6c01d3b1fd456ea9a1
harinez2 <yonishi@student.42tokyo.jp>
2021-09-04 17:16:19 +0900

    ctrl+c fix

--- srcs/signal.c
+++ srcs/signal.c
@@ -3,9 +3,12 @@
 void	sigint_handler(int signum)
 {
 	(void)signum;
+	rl_replace_line("", 0);
+	rl_on_new_line();
 	write(1, &BACK_CURSOR, ft_strlen(BACK_CURSOR));
 	write(1, &CLEAR_FROM_CURSOR, ft_strlen(CLEAR_FROM_CURSOR));
-	write(1, &"\n"SHELL_PROMPT, ft_strlen("\n"SHELL_PROMPT));
+	write(1, &"\n", 1);
+	rl_redisplay();
 }
 
 void	set_signal(void)
