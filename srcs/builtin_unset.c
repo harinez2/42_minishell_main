8e7f38fff5cd3ef0e7ec897a39dd8dd94084489c
harinez2 <yonishi@student.42tokyo.jp>
2021-09-09 17:54:30 +0900

    return code fix for unset

--- srcs/builtin_unset.c
+++ srcs/builtin_unset.c
@@ -3,15 +3,20 @@
 int	builtincmd_unset(t_arg *arg, t_cmd *cmd)
 {
 	int		i;
+	int		ret;
 
 	dbg_print_cmdstart(arg, cmd->param[0]);
 	dbg_print_str(arg, "=== builtin cmd unset ===\n");
+	ret = 0;
 	i = 1;
 	while (i < cmd->param_cnt)
 	{
 		if ((size_t)ft_strchr(cmd->param[i], '=') != ft_strlen(cmd->param[i]))
+		{
 			print_custom_error(
 				ERR_NOT_VALID_IDENTIFIER, cmd->param[0], cmd->param[i]);
+			ret = 1;
+		}
 		else
 		{
 			delete_env_from_envlst(&arg->envlst, cmd->param[i]);
@@ -19,5 +24,5 @@ int	builtincmd_unset(t_arg *arg, t_cmd *cmd)
 		}
 		i++;
 	}
-	return (0);
+	return (ret);
 }
